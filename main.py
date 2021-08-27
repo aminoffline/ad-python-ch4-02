a9570e8b1326c66db1bd5c24083891f8feb1e04a
Amin <aminoffline@yahoo.com>
2021-08-28 02:03:32 +0430

    PEP8

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import unicodecsv
 from bs4 import BeautifulSoup
-import requests,csv
-csv_file = open('results.csv','w',encoding='utf-8')
+import requests
+import csv
+csv_file = open('results.csv', 'w', encoding='utf-8')
 csv.writer = csv.writer(csv_file)
-csv.writer.writerow(['Ad Title ','Ad Description'])
+csv.writer.writerow(['Ad Title ', 'Ad Description'])
 
 price = 'توافقی'
-response = requests.get('https://divar.ir/s/shiraz',timeout= 5).text
-soup = BeautifulSoup(response,'lxml')
+response = requests.get('https://divar.ir/s/shiraz', timeout=5).text
+soup = BeautifulSoup(response, 'lxml')
 
-for post in soup.find_all('div',class_='post-card-item kt-col-6 kt-col-xxl-4'):
+for post in soup.find_all('div', class_='post-card-item kt-col-6 kt-col-xxl-4'):
     try:
-        title = post.find('div',class_='kt-post-card__title').text
-        description = post.find('div',class_='kt-post-card__description').text
+        title = post.find('div', class_='kt-post-card__title').text
+        description = post.find('div', class_='kt-post-card__description').text
         if price in description:
-            csv.writer.writerow([title,description])
-            print(title,description)
+            csv.writer.writerow([title, description])
+            print(title, description)
     except:
         pass
 
