41071ccd0a86535b4f29937dc5992f7a0d1af86c
Amin <aminoffline@yahoo.com>
2021-08-28 01:43:51 +0430

    works but need more work to be better

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import unicodecsv
 from bs4 import BeautifulSoup
 import requests,csv
-csv_file = open('results.csv','w')
+csv_file = open('results.csv','w',encoding='utf-8')
 csv.writer = csv.writer(csv_file)
-csv.writer.writerow(['Ad Title','Ad Description'])
-'''
-post-card-item kt-col-6 kt-col-xxl-4
-	kt-post-card__description
-	<div class="kt-post-card__description">توافقی</div>
-	'''
+csv.writer.writerow(['Ad Title ','Ad Description'])
+
 price = 'توافقی'
 response = requests.get('https://divar.ir/s/shiraz',timeout= 5).text
 soup = BeautifulSoup(response,'lxml')
 
-post = soup.find('div',class_='post-card-item kt-col-6 kt-col-xxl-4')
-title = post.find('div',class_='kt-post-card__title').text
-description = post.find('div',class_='kt-post-card__description').text
-if price in description:
-    csv.writer.writerow([title,description])
-print(description, post.prettify())
-
-
-
-
+for post in soup.find_all('div',class_='post-card-item kt-col-6 kt-col-xxl-4'):
+    try:
+        title = post.find('div',class_='kt-post-card__title').text
+        description = post.find('div',class_='kt-post-card__description').text
+        if price in description:
+            csv.writer.writerow([title,description])
+            print(title,description)
+    except:
+        pass
 
 csv_file.close()
